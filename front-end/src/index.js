f24e4f4483a505d33d83b1e2a4e56e8a68c07c39
ashlab11 <116689240+ashlab11@users.noreply.github.com>
2024-05-30 00:56:06 -0400

    Delete front-end directory

--- front-end/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app'; 
-
-
-const rootElement = document.getElementById('root');
-ReactDOM.createRoot(rootElement).render(<App />);
